1d67b493a66d6cf3438e4e930c07d4234d01fc8d
tjy0120 <1113062407@qq.com>
2023-04-11 22:43:14 +0800

    add read dataTable

---
+++ GDDataStatistics/ExcelDataFactory.cs
@@ -0,0 +1,78 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Data;
+using System.IO;
+
+namespace GDDataStatistics
+{
+    public class ExcelDataFactory
+    {
+        /// <summary>
+        /// 通过地址加载excel数据
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static DataTable LoadExcelData(string filePath)
+        {
+            var dt = new DataTable();
+            using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var hssfworkbook = new XSSFWorkbook(file);
+                var sheet = hssfworkbook.GetSheetAt(0);
+                //for (var j = 0; j < 5; j++)
+                //{
+                //    dt.Columns.Add(Convert.ToChar(((int)'A') + j).ToString());
+                //}
+                var rows = sheet.GetRowEnumerator();
+                while (rows.MoveNext())
+                {
+                    var row = (XSSFRow)rows.Current;
+                    var dr = dt.NewRow();
+                    for (var i = 0; i < row.LastCellNum; i++)
+                    {
+                        var cell = row.GetCell(i);
+                        if (cell == null)
+                        {
+                            dr[i] = null;
+                        }
+                        else
+                        {
+                            switch (cell.CellType)
+                            {
+                                case CellType.Blank:
+                                    dr[i] = "[null]";
+                                    break;
+                                case CellType.Boolean:
+                                    dr[i] = cell.BooleanCellValue;
+                                    break;
+                                case CellType.Numeric:
+                                    dr[i] = cell.ToString();
+                                    break;
+                                case CellType.String:
+                                    dr[i] = cell.StringCellValue;
+                                    break;
+                                case CellType.Formula:
+                                    try
+                                    {
+                                        dr[i] = cell.NumericCellValue;
+                                    }
+                                    catch
+                                    {
+                                        dr[i] = cell.StringCellValue;
+                                    }
+                                    break;
+                                case CellType.Error:
+                                    break;
+                                default:
+                                    dr[i] = "=" + cell.CellFormula;
+                                    break;
+                            }
+                        }
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+    }
+}
--- GDDataStatistics/Form1.cs
+++ GDDataStatistics/Form1.cs
@@ -62,7 +62,26 @@ namespace GDDataStatistics
                 //返回指定目录中的文件的名称（绝对路径）
                 string[] files = System.IO.Directory.GetFiles(dialog.SelectedPath);
                 //获取Test文件夹下所有文件名称
-                string[] files2 = System.IO.Directory.GetFiles(dialog.SelectedPath, "*.xlsx", System.IO.SearchOption.AllDirectories);
+                List<string> filePaths = System.IO.Directory.GetFiles(dialog.SelectedPath, "*.xlsx", System.IO.SearchOption.AllDirectories).ToList();
+
+                if (filePaths != null && filePaths.Count > 0)
+                {
+                    MessageBox.Show($"当前文件夹有:{filePaths.Count()}个文件准备开始处理");
+                    foreach (var filePath in filePaths)
+                    {
+                        ShowInfo($"开始处理文件：{filePath}。");
+
+                        ExcelDataFactory.LoadExcelData(filePath);
+
+                        ShowInfo($"文件：{filePath}处理完成。");
+                    }
+
+                    MessageBox.Show("所有文件处理完成，请查看导出结果");
+                }
+                else
+                {
+                    MessageBox.Show("当前路径下没有可执行的Excel文件，请确认路径是否选错");
+                }
             }
 
         }
@@ -75,7 +94,8 @@ namespace GDDataStatistics
 
         public void ShowInfo(string msg)
         {
-            this.BeginInvoke((Action)(()=>{
+            this.BeginInvoke((Action)(() =>
+            {
                 this.log.AppendText($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}  {msg}");
                 this.log.AppendText(Environment.NewLine);
                 this.log.ScrollToCaret();
